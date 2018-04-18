c3cf942f27e342171173acaa562b9e6dce1d68d1
Arvind Sharma <avin001@gmail.com>
2018-04-18 20:37:28 +0530

    Added new file plus_vs_plus_equals_to.py
    Added new file plus_vs_plus_equals_to.py

---
+++ plus_vs_plus_equals_to.py
@@ -0,0 +1,13 @@
+import numpy as np
+
+a = np.array([1, 2, 3, 4])
+b = a
+a += np.array([1, 1, 1, 1])
+print(b)
+
+# Code Snippet 2
+
+a = np.array([1, 2, 3, 4])
+b = a
+a = a + np.array([1, 1, 1, 1])
+print(b)
